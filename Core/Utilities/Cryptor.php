13311e1a9bc04c0e4c6a630e969b9ec0fb976924
Scott Markoski <s.markoski@gmail.com>
2012-02-19 16:42:03 -0500

    Added an encryption class. Stubbed so far. Need to fill out more, and write tests.

---
+++ Core/Utilities/Cryptor.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Cryptor
+ * 
+ * The cryptor class provides encryption/decryption functionality using PHP's 
+ * mcrypt. It also provides functionality for things such as secure strings. Secure
+ * strings are strings where the data is plainly visible, but that can be verified
+ * for authenticity.
+ *
+ * @author smarkoski
+ */
+class Cryptor {
+    
+    public function __construct($key){
+        
+    }
+    
+    
+}
+
+?>

3a8f638a8faec1b24640e84a69fbe03fe8dcd37c
Scott Markoski <s.markoski@gmail.com>
2012-07-10 10:26:49 -0400

    I have the alert indicator icons working almost exactly as I want. They are grouped into groups of 10, 5 or singles. This is progress on issue #2.

--- App/Views/alertindicator.view.php
+++ App/Views/alertindicator.view.php
@@ -1,4 +1,18 @@
-<?php foreach ($alerts as $alert): ?>
-<span>&block;</span>
+<?php $tenchunks = array_chunk($alerts, 10); ?>
+<?php foreach ($tenchunks as $tenChunk): ?>
+    <?php if (count($tenChunk) == 10): ?>
+        <span class="alertIndicatorIcon tenGroup">&block;</span>
+    <?php else: ?>
+        <?php $fivechunks = array_chunk($tenChunk, 5); ?>
+        <?php foreach ($fivechunks as $fiveChunk): ?>
+            <?php if (count($fiveChunk) == 5): ?>
+                <span class="alertIndicatorIcon fiveGroup">&block;</span>
+            <?php else: ?>
+                <?php foreach ($fiveChunk as $alert): ?>
+                    <span class="alertIndicatorIcon">&block;</span>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
 <?php endforeach; ?>
 
